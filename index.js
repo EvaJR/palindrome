dc04ace12d1b77d721554af5d366f6e32b425e00
Eva van Rooden <eva.van.rooden@capgemini.com>
2019-02-19 11:21:31 +0100

    initialize repo

---
+++ index.js
@@ -0,0 +1,32 @@
+// Reverses a string.
+function reverse(string) {
+  return string.split("").reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  }
+
+  // Makes the phrase LOUDER.
+  this.louder = function() {
+    return this.content.toUpperCase();
+  };
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+}
+
+TranslatedPhrase.prototype = new Phrase(); // js inheritance
